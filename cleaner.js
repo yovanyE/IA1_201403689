7efbe2378f6a5cea88bd8dded893815c47b5d241
yovanyE <syovany@gmail.com>
2025-02-07 23:54:43 -0600

    correciones

--- cleaner.js
+++ cleaner.js
@@ -5,38 +5,46 @@ function reflex_agent(location, state) {
 }
 
 function test(states, visitedStates) {
-    var location = states[0];
-    var state = location === "A" ? states[1] : states[2];
-    var action_result = reflex_agent(location, state);
 
-    // Ejecutar la acción dependiendo del resultado
-    if (action_result === "CLEAN") {
-        if (location === "A") {
-            states[1] = "CLEAN";
-        } else if (location === "B") {
-            states[2] = "CLEAN";
-        }
-    } else if (action_result === "RIGHT") {
-        states[0] = "B";
-    } else if (action_result === "LEFT") {
-        states[0] = "A";
+    const currentState =JSON.stringify(states);
+
+    if (!visitedStates.has(currentState)) {
+        visitedStates.add(currentState);
+        document.getElementById("log").innerHTML += `<br>Visited State: ${currentState}`;
     }
-
-    // Registrar el estado después de la acción
-    const currentState = states.join();
-    visitedStates.add(currentState);
-    document.getElementById("log").innerHTML += `<br>Visited State: ${currentState}`;
 
     // Detener el proceso si todos los estados posibles han sido visitados
     if (visitedStates.size === 8) {
         document.getElementById("log").innerHTML += "<br>All states visited! System halted.";
         return;
     }
-    console.log(visitedStates.size);
-    // Llamar a la función nuevamente después de 3 segundos
+
+    var location = states[0];
+    var state = location === "A" ? states[1] : states[2];
+    var action_result = reflex_agent(location, state);
+
+    // Revisar si no queda ninguna acción relevante por hacer
+
+    document.getElementById("log").innerHTML += `<br>Location: ${location} | Action: ${action_result}`;
+
+    if (action_result === "CLEAN") {
+        if (location === "A") { 
+            states[1] = "CLEAN";
+        }
+        else if (location === "B") {
+            states[2] = "CLEAN";
+        }
+    } else if (action_result === "RIGHT"){
+        states[0] = "B";
+    } 
+    else if (action_result === "LEFT")
+    { 
+        states[0] = "A";
+    }
+
     setTimeout(() => {
         test(states, visitedStates);
-    }, 3000);
+    }, 2000);
 }
 
 var states = ["A", "DIRTY", "DIRTY"];
