e437009b7e37411a064a94544aa27da754a27219
yovanyE <syovany@gmail.com>
2025-04-01 19:25:16 -0600

    se agrega HT3

--- cleaner.js
+++ cleaner.js
@@ -4,49 +4,50 @@ function reflex_agent(location, state) {
     else if (location === "B") return "LEFT";
 }
 
-function test(states, visitedStates) {
+function test(statesCurrent, visitedStates) {
+    const currentState = states.join();
 
-    const currentState =JSON.stringify(states);
-
-    if (!visitedStates.has(currentState)) {
-        visitedStates.add(currentState);
+    if (!visitedStates.includes(currentState)) {
+        visitedStates.push(currentState);
         document.getElementById("log").innerHTML += `<br>Visited State: ${currentState}`;
     }
 
     // Detener el proceso si todos los estados posibles han sido visitados
-    if (visitedStates.size === 8 || visitedStates.size==5) {
+    if (visitedStates.length === 8) {
         document.getElementById("log").innerHTML += "<br>All states visited! System halted.";
         return;
     }
 
-    var location = states[0];
-    var state = location === "A" ? states[1] : states[2];
+    var location = statesCurrent[0];
+    var state = location === "A" ? statesCurrent[1] : statesCurrent[2];
     var action_result = reflex_agent(location, state);
-
-    // Revisar si no queda ninguna acción relevante por hacer
 
     document.getElementById("log").innerHTML += `<br>Location: ${location} | Action: ${action_result}`;
 
     if (action_result === "CLEAN") {
-        if (location === "A") { 
-            states[1] = "CLEAN";
+        if (location === "A") {
+            statesCurrent[1] = "CLEAN";
+        } else if (location === "B") {
+            statesCurrent[2] = "CLEAN";
         }
-        else if (location === "B") {
-            states[2] = "CLEAN";
+    }else if(action_result=="DIRTY"){
+        if (location === "A") {
+            statesCurrent[1] = "DIRTY";
+        } else if (location === "B") {
+            statesCurrent[2] = "DIRTY";
         }
-    } else if (action_result === "RIGHT"){
-        states[0] = "B";
     } 
-    else if (action_result === "LEFT")
-    { 
-        states[0] = "A";
+    else if (action_result === "RIGHT") {
+        statesCurrent[0] = "B";
+    } else if (action_result === "LEFT") {
+        statesCurrent[0] = "A";
     }
-
+    console.log(visitedStates);
     setTimeout(() => {
-        test(states, visitedStates);
-    }, 2000);
+        test(statesCurrent, visitedStates);
+    }, 1000);
 }
 
 var states = ["A", "DIRTY", "DIRTY"];
-var visitedStates = new Set();
+var visitedStates = [];
 test(states, visitedStates);
